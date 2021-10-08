1826001356f1a21bf8495310c7e802e121683982
ammar <ammarraneez@gmail.com>
2021-10-09 01:01:30 +0530

    feat: reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const User = require('../models/User');
 const ErrorResponse = require('../utils/errorResponse');
 const sendEmail = require('../utils/sendEmail');
@@ -111,8 +112,34 @@ exports.forgotPassword = async (req, res, next) => {
   }
 }
 
-exports.resetPassword = (req, res, next) => {
-  res.send('Reset password route');
+exports.resetPassword = async (req, res, next) => {
+  const resetPasswordToken = crypto.createHash('sha256').update(req.params.resetToken).digest('hex');
+
+  try {
+    const user = await User.findOne({
+      resetPasswordToken,
+
+      // check if the reset password url is still valid
+      resetPasswordExpire: { $gt: Date.now(), }
+    });
+
+    if (!user) {
+      return next(new ErrorResponse('Invalid Reset Token', 400));
+    }
+
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+    
+    res.status(201).json({
+      success: true,
+      data: 'Password Reset Request',
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 const sendToken = (user, statusCode, res) => {
